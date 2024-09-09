2932f10f80f1fda16ceb82d708cf1edcbb6a1eef
Archie-study <archiestudy12@gmail.com>
2024-09-09 22:25:48 +0700

    Preparation of React Native

---
+++ src/screens/HelloScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HelloScreen = () => {
+    return (
+        <View>
+            <Text>
+                Hello Everyone!
+            </Text>
+        </View>
+    )
+}
+
+export default HelloScreen;
